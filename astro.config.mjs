67e9f5fdffc27621e31a2915e55710c020d34854
Vladyslav Tkachenko <vlad@wtf.lv>
2023-07-25 14:59:37 +0300

    adding base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://turn.github.io',
-  // base: '/astro-docs',
+  site: "https://turn.github.io",
+  base: "/astro-docs",
   integrations: [
     starlight({
       title: "My Docs",
