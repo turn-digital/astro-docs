e0e34f2150937c29ccb2fcb04a4dbc877fa564dd
Vlad <thevlad2698@gmail.com>
2023-10-20 14:03:30 +0300

    adding new sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://turn.github.io",
+  site: "https://vadlinijas.pieklustamiba.lv/",
   integrations: [
     starlight({
       title: "Piekļūstamības vadlīnijas",
